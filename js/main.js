705cff384bb75741088b0246b857e853594619d7
Ivan <ivan.montiel@nextiva.com>
2019-04-26 12:41:03 -0700

    Fix appending anchor

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 window.onload = function () {
   var target = window.document.getElementById('target');
 
-
   var anchor = document.createElement('a');
   anchor.href = 'https://google.com';
+  anchor.id = 'anchor-target';
 
   anchor.onclick = function (e) {
     e.preventDefault();
 
     window.open('http://google.com', '_blank');
   }
+
+  target.appendChild(anchor);
 }
